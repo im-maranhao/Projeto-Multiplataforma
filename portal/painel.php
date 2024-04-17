989c40e262b914cccda1563589b3a5a986061f64
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2024-04-16 23:15:42 -0300

    Atualizar o painel.php

--- portal/painel.php
+++ portal/painel.php
@@ -4,6 +4,7 @@ include('verifica_login.php');
 include('menu.php');
 include('dashboard.php')
 ?>
+
 <!DOCTYPE html> 
 <html>
 <head>
@@ -14,8 +15,8 @@ include('dashboard.php')
 <link rel="stylesheet" href="../assets/css/painel.css">
      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="shortcut icon" href="../assets/imagem/favicon.ico" type="image/x-icon">
-
 </head>
+
 <body >
 
     <header> 
